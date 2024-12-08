37a64febd6e54f3b48172e8c6b5d0fc9678755c5
muhammederem <muhammederemm@gmail.com>
2024-12-09 00:13:18 +0300

    added text display

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -81,28 +81,43 @@ class BlipDisplayNode:
         return image, formatted_output
 
 
-class StringPairDisplayNode:
-
+class ShowText:
     @classmethod
-    def INPUT_TYPES(cls):
+    def INPUT_TYPES(s):
         return {
             "required": {
-                "string_pairs": ("LIST_STRING", {"multiline": True}),  # List of question-answer pairs
-            }
+                "text": ("STRING", {"forceInput": True}),
+            },
+            "hidden": {
+                "unique_id": "UNIQUE_ID",
+                "extra_pnginfo": "EXTRA_PNGINFO",
+            },
         }
 
-    RETURN_TYPES = ("STRING",)  # Outputs a single formatted string
-    FUNCTION = "process"
-    OUTPUT_NODE = True  # Terminal node
+    INPUT_IS_LIST = True
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "notify"
+    OUTPUT_NODE = True
+    OUTPUT_IS_LIST = (True,)
+
     CATEGORY = "Blip"
 
-    def process(self, string_pairs):
-        """
-        Display a formatted version of string pairs.
+    def notify(self, text, unique_id=None, extra_pnginfo=None):
+        if unique_id is not None and extra_pnginfo is not None:
+            if not isinstance(extra_pnginfo, list):
+                print("Error: extra_pnginfo is not a list")
+            elif (
+                not isinstance(extra_pnginfo[0], dict)
+                or "workflow" not in extra_pnginfo[0]
+            ):
+                print("Error: extra_pnginfo[0] is not a dict or missing 'workflow' key")
+            else:
+                workflow = extra_pnginfo[0]["workflow"]
+                node = next(
+                    (x for x in workflow["nodes"] if str(x["id"]) == str(unique_id[0])),
+                    None,
+                )
+                if node:
+                    node["widgets_values"] = [text]
 
-        :param string_pairs: List of strings where each contains a question-answer pair.
-        :return: A single formatted string combining all pairs.
-        """
-        # Format the question-answer pairs for better readability
-        formatted_output = "\n\n".join(string_pairs)
-        return (formatted_output,)
+        return {"ui": {"text": text}, "result": (text,)}
