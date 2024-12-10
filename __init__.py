0d530e3c8c54a975c9a9ede982cfa8da2bb33563
muhammederem <muhammederemm@gmail.com>
2024-12-10 14:26:29 +0300

    refactor: update loading message for Blip Nodes

--- __init__.py
+++ __init__.py
@@ -4,4 +4,4 @@ NODE_CLASS_MAPPINGS = {
     "Blip Processor Node": BlipProcessorNode,
     }
     
-print("\033[34mComfyUI Tutorial Nodes: \033[92mLoaded\033[0m")
+print("\033[34mBlip Nodes: \033[92mLoaded\033[0m")
